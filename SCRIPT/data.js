62d48d6fda7f37caf94a81bc8827e49fec4e541a
health-lover <118627636+health-lover@users.noreply.github.com>
2022-11-22 11:11:24 -0300

    Update data.js

--- SCRIPT/data.js
+++ SCRIPT/data.js
@@ -1,7 +1,7 @@
 var defaultThreads = [
     {
         id: 1,
-        title: "Thread 1",
+        title: "Dúvidas",
         author: "Ana",
         date: Date.now(),
         content: "Thread content",
@@ -9,31 +9,69 @@ var defaultThreads = [
             {
                 author: "Jack",
                 date: Date.now(),
-                content: "Oi"
+                content: "O que é bulimia?"
             },
             {
                 author: "Arthur",
                 date: Date.now(),
-                content: "Oie"
+                content: "Transtorno alimentar grave marcado por compulsão, seguido de métodos para evitar o ganho de peso."
             }
         ]
     },
     {
         id: 2,
-        title: "Thread 2",
+        title: "Sugestões",
         author: "Ana",
         date: Date.now(),
         content: "Thread content 2",
         comments: [
             {
-                author: "Jack",
+                author: "Júlia",
                 date: Date.now(),
-                content: "Tudo bem?"
+                content: "Vcs deveriam colocar mais tópicos."
             },
             {
                 author: "Arthur",
                 date: Date.now(),
-                content: "Eai! Tudo e vc?"
+                content: "Vamos dar uma olhada"
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Opiniões",
+        author: "Ana",
+        date: Date.now(),
+        content: "Thread content 3",
+        comments: [
+            {
+                author: "Jack",
+                date: Date.now(),
+                content: "Ainda tem gente que pensa que isso é besteira"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Verdade"
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Geral",
+        author: "Ana",
+        date: Date.now(),
+        content: "Thread content 4",
+        comments: [
+            {
+                author: "Luisa",
+                date: Date.now(),
+                content: "Eai galera!"
+            },
+            {
+                author: "Arthur",
+                date: Date.now(),
+                content: "Eai! blz?"
             }
         ]
     }
